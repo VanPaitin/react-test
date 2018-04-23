b1ee381b4a728cdaa64d54e4f9f8ee3dfe1fa3cc
Mayowa Pitan <mayowa.pitan@andela.com>
2018-04-23 11:02:58 +0100

    complete styling of calendar component

--- src/Calendar.js
+++ src/Calendar.js
@@ -37,21 +37,24 @@ export default class Calendar extends React.Component {
   };
 
   render() {
-    let weekday = new Array(7);
-    weekday[0] =  "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let date = new Date();
+    let today = date.getDay();
+    let twoDaysTime = (today + 2) % weekdays.length
+
     return (
       <div className="calendar container-fluid">
         <div className="header">
           <h4>Peek into the future</h4>
           <Glyphicon glyph="calendar" onClick={this.props.calendarDisplay}/>
         </div>
-        <Button>Today</Button> <Button>Tomorrow</Button>
+
+        <div className="day-group">
+          <Button onClick={this.props.calendarDisplay} active>Today</Button>
+          <Button>Tomorrow</Button>
+          <Button>{weekdays[twoDaysTime]}</Button>
+        </div>
+
         <FullCalendar
           Select={Select}
           fullscreen={false}
